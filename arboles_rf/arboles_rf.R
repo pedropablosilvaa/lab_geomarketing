8b7a91f83682e618382c1ccd233fdb0f2b47f8f1
Pedro <pedropablosilvaa@gmail.com>
2020-08-14 14:02:39 -0400

    random forest

--- arboles_rf/arboles_rf.R
+++ arboles_rf/arboles_rf.R
@@ -1,14 +1,14 @@
 library(rpart)
 library(rpart.plot)
 library(readr)
-
+library(randomForest)
 
 #se define el directorio de trabajo, cambiar para cada caso
-workdir_path <- "~/lab_geomarketing"
+workdir_path <- "~/lab_geomarketing/arboles_rf"
 setwd(workdir_path)
 
 #se lee la base de datos en formato csv
-casen <- read.csv(file = "data/casen_mod.csv")
+casen_mod <- read.csv(file = "data/casen_mod.csv")
 
 
 #trabsformar a factor
@@ -21,7 +21,7 @@ casen_reg11 = casen_mod[(casen_mod$region == 11),]
 
 casen_rm = casen_mod[(casen_mod$region == 13),]
 
-#Arboles
+##---------------- Decision trees  ----------------##
 
 ## Reg 11
 
@@ -41,3 +41,19 @@ rpart.plot(arbol_rm,type=5,extra=104)
 ### Con nac
 arbol_rm_nac=rpart(ing_nivel~edad+esc+sexo+nac,casen_rm,model=T, method = "class")
 rpart.plot(arbol_rm_nac,type=5,extra=104)
+
+
+
+##----------------- Random Forest ----------------##
+
+# Se divide la poblacion en muestra de entrenamiento y evaluacion
+dt = sort(sample(nrow(casen_mod), nrow(casen_mod)*.7))
+casen_train<-casen_mod[dt,]
+casen_test<-casen_mod[-dt,]
+
+
+ingresos_rf = randomForest(ing_nivel~edad+esc+sexo+nac, data = casen_train, ntree = 500)
+
+pred_rf  = predict(ingresos_rf, casen_test)
+
+aciertos_rf = which(casen_test$ing_nivel==pred_rf)
